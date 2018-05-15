ebfff40025ae83effb0117dea27a2e37b761cb58
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2018-05-15 11:34:08 +0800

    Implement dynamic template creation and insertion

--- src/app/plain-dynamic-template-creation/base-three/base-three.component.ts
+++ src/app/plain-dynamic-template-creation/base-three/base-three.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EmbeddedViewRef,
+  Inject,
+  OnDestroy,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-base-three',
@@ -6,16 +16,41 @@ import { Component, OnInit } from '@angular/core';
     <p>
       Base Three Component
     </p>
+
+    <ng-template #dummyThree>
+      <p>I am a template contained in an ng-template.</p>
+      <p>Another paragraph</p>
+    </ng-template>
   `,
   styles: [`
     :host { display: block; }
   `]
 })
-export class BaseThreeComponent implements OnInit {
+export class BaseThreeComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('dummyThree') dummyThree: TemplateRef<any>;
 
-  constructor() { }
+  private _embeddedView: EmbeddedViewRef<any>;
 
-  ngOnInit() {
+  constructor(
+    private _vcr: ViewContainerRef,
+    @Inject(DOCUMENT) private _doc: Document,
+  ) { }
+
+  ngAfterViewInit() {
+    // create an embedded view for the TemplateRef
+    this._embeddedView = this._vcr.createEmbeddedView(this.dummyThree);
+
+    // move the view to the document's body element by appending all existing root nodes of the template.
+    this._embeddedView.rootNodes.forEach(rootNode => this._doc.body.appendChild(rootNode));
+  }
+
+  ngOnDestroy() {
+    // remove the dynamically injected template from the DOM
+    const index = this._vcr.indexOf(this._embeddedView);
+
+    if (index > -1) {
+      this._vcr.remove(index);
+    }
   }
 
 }
